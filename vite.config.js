09489f5143e8c9f1268c910b3339477f4abdc1d3
Rodrigo Coelho <rodrigocoeio@gmail.com>
2022-10-13 05:49:34 -0300

    Changed application running port

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,10 @@ const path = require('path');
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    host: "0.0.0.0",
+    port: 80,
+  },
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
