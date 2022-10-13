41b18c4d6779c6dd0bce8e5446a714d8ee83850f
Rodrigo Coelho <rodrigocoeio@gmail.com>
2022-10-13 05:49:51 -0300

    Added route to GuessWhatView

---
+++ src/views/GuessWhatView.vue
@@ -0,0 +1,13 @@
+<template>
+    <guess-what></guess-what>
+  </template>
+  
+  <script>
+  import GuessWhat from "#/GuessWhat.vue";
+  
+  export default {
+    components: {
+      GuessWhat
+    }
+  }
+  </script>
